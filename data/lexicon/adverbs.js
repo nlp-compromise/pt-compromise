91dd4370754c0845344d651fbf762a0d55f739d9
spencer kelly <spencerkelly86@gmail.com>
2023-02-08 12:58:23 -0500

    results done

--- data/lexicon/adverbs.js
+++ data/lexicon/adverbs.js
@@ -6,4 +6,19 @@ export default [
   'quase',
   'claramente',
   'naturalmente',
+
+  // https://pt.wiktionary.org/wiki/Ap%C3%AAndice:Adv%C3%A9rbios_do_portugu%C3%AAs
+  'certamente', 'deveras', 'efetivamente', 'decerto', 'incontestavelmente', 'realmente', 'seguramente', 'sim',
+  'porque', 'porquê', 'conseguintemente', 'consequentemente',
+  'acaso', 'certamente', 'certo', 'decerto', 'porventura', 'possivelmente', 'provavelmente', 'quiçá', 'talvez',
+  'apenas', 'exclusivamente', 'salvo', 'senão', 'simplesmente', 'só', 'somente', 'unicamente',
+  'ainda', 'até', 'inclusivamente', 'mesmo', 'nomeadamente', 'também',
+  'apenas', 'assaz', 'bastante', 'bem', 'como', 'completamente', 'demais', 'demasiado', 'demasiadamente', 'excessivamente', 'extremamente', 'grandemente', 'intensamente', 'levemente', 'ligeiramente', 'mais', 'mal', 'meio', 'menos', 'mui', 'muito', 'nada', 'pouco', 'profundamente', 'quanto', 'quão', 'quase', 'que', 'tanto', 'tão', 'todo', 'tudo',
+  'onde', 'como', 'quando', 'porque',
+  'abaixo', 'acima', 'acolá', 'adiante', 'afora', 'aí', 'além', 'algures', 'alhures', 'ali', 'antes', 'aonde', 'aquém', 'aqui', 'atrás', 'através', 'avante', 'cá', 'debaixo', 'defronte', 'detrás', 'dentro', 'diante', 'donde', 'fora', 'lá', 'longe', 'nenhures', 'onde', 'perto',
+  'adrede', 'alerta', 'aliás', 'assim', 'bem', 'calmamente', 'como', 'corajosamente', 'debalde', 'depressa', 'devagar', 'dificilmente', 'felizmente', 'livremente', 'mal', 'melhor', 'pior', 'principalmente', 'propositadamente', 'quase', 'selvaticamente', 'sobremaneira', 'sobremodo', 'também',
+  'jamais', 'não', 'nem', 'nunca', 'tampouco',
+  'antes', 'depois', 'primeiramente', 'ultimamente',
+  'algo', 'apenas', 'assaz', 'bastante', 'demasiado', 'mais', 'menos', 'muito', 'nada', 'pouco', 'quanto', 'quão', 'quase', 'tanto', 'tão', 'todo', 'tudo',
+  'afinal', 'agora', 'ainda', 'amanhã', 'amiúde', 'anteontem', 'antes', 'antigamente', 'aqui', 'breve', 'brevemente', 'cedo', 'comumente', 'concomitantemente', 'dantes', 'depois', 'diariamente', 'doravante', 'enfim', 'então', 'entrementes', 'finalmente', 'hoje', 'imediatamente', 'já', 'jamais', 'logo', 'nunca', 'ontem', 'ora', 'outrora', 'presentemente', 'primeiro', 'quando', 'raramente', 'sempre', 'simultaneamente', 'tarde',
 ]
